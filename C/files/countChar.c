e1e8d9d937cd57a7e4964d45b94037d9b0ad420f
pmihsan <ihsanpmh@gmail.com>
2022-01-10 21:38:37 +0530

    Organised  C folder

--- C/files/countChar.c
+++ C/files/countChar.c
@@ -10,27 +10,22 @@ int main()
 	scanf("%s",path);
 	file=fopen(path,"r");
 	characters=words=lines=0;
-		while((ch = fgetc(file) != EOF))
-		{
-			characters++;
+	while((ch = fgetc(file) != EOF)){
+	characters++;
 	if ((ch =='\n'|| ch == '\0')){
 		lines++;
 	}
 	if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0' ){
 		words++;
-		}
-		}
-
-	printf("Inside If  words =%d\n",words);
-	if( characters>0)
+	}
+	}
+	if(characters>0)
 	{
 		words++;
 		lines++;
 	}
-	printf(" words =%d\n",words);
-	printf("\n");
 	printf("Total Characters = %d\n", characters);
-	printf("Total  words =%d\n",words);
+	printf("Total  words =%d\n",words+1);
 	printf("Total lines=%d\n",lines);
 	fclose(file);
 	return 0;
