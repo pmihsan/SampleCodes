10a8542873bf70534133690c2abb05672ce13c41
pmihsan <ihsanpmh@gmail.com>
2022-01-10 19:09:56 +0530

    Created Basic Data Types in Python

---
+++ Python/DataTypes/basicDataTypes.py
@@ -0,0 +1,67 @@
+#Number 
+
+#integer
+a = 12
+print(a)
+
+#float
+b = 12.23
+print(b)
+
+#complex
+c = 2+3j 
+print(c) 
+
+#Boolean
+
+#bool
+ans = True
+print(ans)
+
+#bytes
+by = b"Hello"
+print(by) 
+
+#byteArray
+byArr = bytearray(5)
+print(byArr)
+
+
+#memoryView
+memView = memoryview(bytes(5))
+print(memView)
+
+#String
+
+word = "hello"
+print(word)
+type(word)
+
+#Complex Data Types
+
+#range
+x = range(6) 
+print(x) 
+
+#list
+arr = [1, 2, 3, 4, 10, 12] #Similar to array in c
+print(arr)
+
+#tuple
+y = (1, 2, 3) 
+print(y)   
+
+z = (1, 1.2, 3j, "Hello")
+print(z)
+
+#dictionary
+key_pair = { "name" : "John", "age" : 19, "marks" : 94.7}
+print(key_pair)
+
+#set
+fruits = {"apple", "mango", "cherry"}
+print(fruits)
+
+#frozenset
+fruit = frozenset({"apple", "banana", "cherry"})
+print(fruit)
