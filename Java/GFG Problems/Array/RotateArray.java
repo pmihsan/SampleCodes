a73932a9d8bdf08c17b92d894e6eba629c5bad03
pmihsan <89512452+pmihsan@users.noreply.github.com>
2022-02-18 21:35:45 +0530

    Modified RotateArray.java

--- Java/GFG Problems/Array/RotateArray.java
+++ Java/GFG Problems/Array/RotateArray.java
@@ -2,11 +2,28 @@ import java.util.Arrays;
 
 public class RotateArray{
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int n = arr.length;
-        int d = 9;
-        Rotate(arr, d, n);
+        int d = 3;
+        // Rotate(arr, d, n);
+        RotateArr(arr,d,n);
         System.out.println(Arrays.toString(arr));
+    }
+    static void RotateArr(int[] arr, int d, int n){
+        int k = d % n;
+        k = n - k;
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            if(i < k){
+                nums[i] = arr[n + i - k];
+            }
+            else{
+                nums[i] = arr[i - k];
+            }
+        }
+        for(int i=0;i<n;i++){
+            arr[i] = nums[i];
+        }
     }
     static void Rotate(int[] arr, int d, int n){
         int k = d % n;
